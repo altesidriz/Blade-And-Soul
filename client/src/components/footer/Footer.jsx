a718d41aa5d95e48970881568a6f7815d56d7879
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-08-05 09:38:35 +0300

    footer finished

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import styles from './footer.module.css';
 import pegi from '../../assets/footer/pegi.png';
+import ncImg from '../../assets/footer/logo-plaync.png';
 import { FaFacebookF, FaYoutube, FaTwitch } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
@@ -17,6 +18,19 @@ const Footer = () => {
             <div className={styles.pegi}>
                 <img src={pegi} alt="" />
                 <span>PROVISIONAL</span>
+            </div>
+            <div className={styles.separator}></div>
+            <div className={styles.links}>
+                <a href="#">Careers</a>
+                <a href="#">Legal Documentation</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Cookie Policy</a>
+            </div>
+            <div className={styles.nc}>
+                <div className={styles.logoNc}>
+                    <img src={ncImg} alt="" />
+                </div>
+                <span>&copy; NCSOFT Corporation. All rights reserved.</span>
             </div>
         </div>
     );
